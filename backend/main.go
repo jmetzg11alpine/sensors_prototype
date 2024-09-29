5fe4a041c3535c688f0f1197b8ffe378c3765230
jmetzg11alpine <jmetzg11@gmail.com>
2024-09-29 11:15:09 -0700

    added platformio esp32 project

--- backend/main.go
+++ backend/main.go
@@ -10,7 +10,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{""},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
 		ExposeHeaders:    []string{"Content-Length"},
